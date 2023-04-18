875ac3bdf2edeb143a81dd43ab96512f995e7bec
ShivanshPratap <shivanshpratap30dec@gmal.com>
2023-04-19 04:53:41 +0530

    PDF Merger

---
+++ pdf merger/main.py
@@ -0,0 +1,14 @@
+#pip install pypdf2
+
+import PyPDF2
+
+pdfiles = ["1.pdf", "2.pdf"]
+merger = PyPDF2.PdfMerger()
+
+for filename in pdfiles:
+        pdfFile = open(filename, 'rb')
+        pdfReader = PyPDF2.PdfReader(pdfFile)
+        pdfMerge.append(pdfReader)
+
+pdfFile.close()
+pdfMerge.write('merged.pdf')
