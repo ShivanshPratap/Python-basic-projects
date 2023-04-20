b6c97a0e65b75a9175de38879e4d0dfb087f9f15
ShivanshPratap <shivanshpratap30dec@gmal.com>
2023-04-21 01:05:30 +0530

    Madlibs

---
+++ Madlibs/Madlibs.py
@@ -0,0 +1,9 @@
+adj = input("Adjective")
+verb1 = input("verb: ")
+verb2 = input("verb: ")
+famous_person = input("Famous Person")
+
+Madlibs = f"Computer programming is so {adj}! It makes me so excited all the time because \
+I love to {verb1}. stay hydarated and {verb2} like you are{famous_person}!"
+
+print(Madlibs)
